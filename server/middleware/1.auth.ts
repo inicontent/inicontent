657ab5315a0a76ee9f15ae0db53621264f9d2a34
kamatil-dev <karim.amahtil@gmail.com>
2023-12-24 00:50:05 +0100

    update vercel test again

--- server/middleware/1.auth.ts
+++ server/middleware/1.auth.ts
@@ -57,12 +57,7 @@ export default defineWrappedResponseHandler(async (event: any) => {
     .pathname.split("/")
     .slice(1);
   if (api !== "api") return;
-  console.log("using CWD:");
-  console.log(readdirSync(process.cwd()));
-
-  console.log("using Runtime:");
-  console.log(readdirSync(useRuntimeConfig().databasePath));
-
+  console.log(readdirSync(join(process.cwd(), "public")));
   if (!DatabaseSlug) throw new Error("db_not_ found");
 
   if (await isExists(join(useRuntimeConfig().databasePath, DatabaseSlug)))
