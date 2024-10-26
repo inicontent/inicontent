7c1ec106aa7e9e9fdbfff38214293b2340086e00
kamatil <karim.amahtil@gmail.com>
2024-10-26 12:26:07 +0100

    fix: redirect to /admin

---
+++ pages/[[database]]/index.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script lang="ts" setup>
+definePageMeta({
+    middleware: ["database", "user", "dashboard"],
+});
+
+const route = useRoute()
+const database = useState<Database>("database");
+await navigateTo(
+    `/${route.params.database ? `${database.value.slug}/` : ""}admin${database.value.slug === 'inicontent' ? '' : '/tables'}`,
+);
+</script>
