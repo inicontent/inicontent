70bbec0cd8312790fb6b413f34b83497c41a2ed3
kamatil <karim.amahtil@gmail.com>
2024-11-25 23:37:43 +0100

    dev: add transition

--- app.vue
+++ app.vue
@@ -3,3 +3,16 @@
         <NuxtPage />
     </App>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+    transition: all 0.4s;
+}
+
+.page-enter-from,
+.page-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
+}
+</style>
