8181beda4f587b2044643624026db137e297335c
kamatil <karim.amahtil@gmail.com>
2024-11-29 16:00:32 +0100

    docs: move comments above section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@ export default defineNuxtConfig({
 	appConfig: {
 		apiBase: "https://api.inicontent.com/",
 		idOne: "d7b3d61a582e53ee29b5a1d02a436d55",
-
-		database: undefined, // use it to overwrite database name
+		// use it to overwrite database name
+		database: undefined,
 	},
 	sourcemap: false,
 	modules: ["@nuxtjs/device", "nuxtjs-naive-ui", "nuxt-tiptap-editor"],
