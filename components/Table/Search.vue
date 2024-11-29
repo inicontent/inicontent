33ccbc3eec58461d7fffaadcb88bd27dff26395b
kamatil <karim.amahtil@gmail.com>
2024-11-29 15:58:53 +0100

    fix: search with table criteria

--- components/Table/Search.vue
+++ components/Table/Search.vue
@@ -52,7 +52,13 @@
                                 @update:value="(v) => item[1] = v" :options="searchSelectOptions(item[3])"
                                 style="width:33.33%" />
                             <Field :model-value="item[2]" @update:modelValue="(v) => {
-                                item[2] = Array.isArray(v) ? v.join(',') : v;
+                                if (v !== undefined) {
+                                    if (isObject(v) && Object.hasOwn(v, 'id'))
+                                        v = v.id
+                                    else if (isArrayOfObjects(v) && v.every(_v => Object.hasOwn(_v, 'id')))
+                                        v = v.map(({ id }) => id);
+                                    item[2] = Array.isArray(v) ? v.join(',') : v;
+                                } else item[2] = undefined
                             }" :field="getFieldFromItem(item)" />
                         </template>
                         <NButton :disabled="items.length === 1" @click="modeValue[condition]?.splice(index, 1)" circle
@@ -64,7 +70,7 @@
                             </template>
                         </NButton>
                     </NInputGroup>
-                    <LazyRenderSearch v-else v-model="(modeValue[condition] as any)[index]" :callback />
+                    <LazyTableSearch v-else v-model="(modeValue[condition] as any)[index]" :callback />
                 </template>
             </NFlex>
         </NCollapseItem>
@@ -73,103 +79,106 @@
 
 <script lang="ts" setup>
 import {
-    IconPlus,
-    IconTrash,
-    IconSwitchHorizontal,
-    IconMinus,
-    IconArrowMerge,
-    IconArrowFork,
+	IconPlus,
+	IconTrash,
+	IconSwitchHorizontal,
+	IconMinus,
+	IconArrowMerge,
+	IconArrowFork,
 } from "@tabler/icons-vue";
-import { getField as getFieldFromSchema } from "inibase/utils";
 import {
-    NButton,
-    NTooltip,
-    NCollapse,
-    NCollapseItem,
-    NDropdown,
-    NFlex,
-    NIcon,
-    NInputGroup,
-    NSelect,
-    NButtonGroup,
+	getField as getFieldFromSchema,
+	isArrayOfObjects,
+	isObject,
+} from "inibase/utils";
+import {
+	NButton,
+	NTooltip,
+	NCollapse,
+	NCollapseItem,
+	NDropdown,
+	NFlex,
+	NIcon,
+	NInputGroup,
+	NSelect,
+	NButtonGroup,
 } from "naive-ui";
 
 defineTranslation({
-    ar: {
-        andGroup: "مجموعة و",
-        orGroup: "مجموعة أو",
-    },
+	ar: {
+		andGroup: "مجموعة و",
+		orGroup: "مجموعة أو",
+	},
 });
 
-const { callback } = defineProps<{ callback: CallableFunction }>()
+const { callback } = defineProps<{ callback: CallableFunction }>();
 
 type searchArrayValueItem =
-    | [string | null, string, any]
-    | [string | null, string, any, any];
+	| [string | null, string, any]
+	| [string | null, string, any, any];
 type searchArrayValue = (searchArrayValueItem | searchArray)[];
 type searchArray = {
-    and?: searchArrayValue;
-    or?: searchArrayValue;
+	and?: searchArrayValue;
+	or?: searchArrayValue;
 };
 const modeValue = defineModel<searchArray>({
-    default: { and: [[null, "=", null]] },
+	default: () => reactive({ and: [[null, "=", null]] }),
 });
 
 const table = useState<Table>("table");
 
 const conditionDropdownOptions = [
-    {
-        key: "and",
-        label: t("andGroup"),
-        icon: () => h(NIcon, () => h(IconArrowMerge))
-    },
-    {
-        key: "or",
-        label: t("orGroup"),
-        icon: () => h(NIcon, () => h(IconArrowFork))
-    },
+	{
+		key: "and",
+		label: t("andGroup"),
+		icon: () => h(NIcon, () => h(IconArrowMerge)),
+	},
+	{
+		key: "or",
+		label: t("orGroup"),
+		icon: () => h(NIcon, () => h(IconArrowFork)),
+	},
 ];
 function toggleCondition(oldCondition: "and" | "or") {
-    modeValue.value[oldCondition === "and" ? "or" : "and"] =
-        modeValue.value[oldCondition];
-    delete modeValue.value[oldCondition];
+	modeValue.value[oldCondition === "and" ? "or" : "and"] =
+		modeValue.value[oldCondition];
+	delete modeValue.value[oldCondition];
 }
 
 function formatItems(items: searchArrayValue) {
-    return items.map((item) => {
-        if (Array.isArray(item) && item[0])
-            item[3] = getFieldFromSchema(item[0], table.value.schema as any);
-        return item;
-    });
+	return items.map((item) => {
+		if (Array.isArray(item) && item[0])
+			item[3] = getFieldFromSchema(item[0], table.value.schema as any);
+		return item;
+	});
 }
 function getFieldFromItem(item: searchArrayValueItem) {
-    return {
-        ...item[3],
-        required: false,
-        labelProps: {
-            showLabel: false,
-            style: "margin-top: -3px;width:33.33%",
-            showFeedback: false,
-        },
-        inputProps: {
-            size: "small",
-            onKeydown: ({ key }: KeyboardEvent) => {
-                if (key === "Enter")
-                    callback()
-            },
-        },
-    };
+	return {
+		...item[3],
+		required: false,
+		labelProps: {
+			showLabel: false,
+			style: "margin-top: -3px;width:33.33%",
+			showFeedback: false,
+		},
+		inputProps: {
+			size: "small",
+			onKeydown: ({ key }: KeyboardEvent) => {
+				if (key === "Enter") callback();
+			},
+		},
+	};
 }
 function searchSelectOptions(field: any): any {
-    return comparisonOperatorOptions().filter(({ value }) => {
-        if (checkFieldType(field.type, ["number", "date"]))
-            return ["=", "!=", ">", ">=", "<", "<="].includes(value);
-        if (
-            checkFieldType(field.type, "array") ||
-            checkFieldType(field.type, "table")
-        )
-            return ![">", ">=", "<", "<="].includes(value);
-        return ![">", ">=", "<", "<=", "[]", "![]"].includes(value);
-    });
+	return comparisonOperatorOptions().filter(({ value }) => {
+		if (checkFieldType(field.type, ["number", "date"]))
+			return ["=", "!=", ">", ">=", "<", "<="].includes(value);
+		if (
+			checkFieldType(field.type, "array") ||
+			checkFieldType(field.type, "table")
+		)
+			return ![">", ">=", "<", "<="].includes(value);
+		return ![">", ">=", "<", "<=", "[]", "![]"].includes(value);
+	});
 }
 </script>
