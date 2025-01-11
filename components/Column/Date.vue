2445a16f8833f546364c938678554ab6a798bc04
kamatil <karim.amahtil@gmail.com>
2025-01-11 17:40:41 +0100

    dev: add tooltip

--- components/Column/Date.vue
+++ components/Column/Date.vue
@@ -1,14 +1,25 @@
 <template>
-    <NTime style="cursor: pointer;" @click="isRelative = !isRelative" :time="Number(value)"
-        :type="isRelative ? 'relative' : 'date'">
-        {{ value }}
-    </NTime>
+    <NTooltip trigger="hover" :delay="500">
+        <template #trigger>
+            <NTime style="cursor: pointer;" @click="isRelative = !isRelative" :time="Number(value)"
+                :type="isRelative ? 'relative' : 'date'">
+                {{ value }}
+            </NTime>
+        </template>
+        {{ t('clickToToggleDate') }}
+    </NTooltip>
 </template>
 
 <script lang="ts" setup>
-import { NTime } from "naive-ui";
+import { NTime, NTooltip } from "naive-ui";
 
 const isRelative = useState("isRelative", () => false);
 
 const { value } = defineProps<{ value?: number }>();
+
+defineTranslation({
+	ar: {
+		clickToToggleDate: "إضغط لتغيير طريقة عرض التاريخ",
+	},
+});
 </script>
