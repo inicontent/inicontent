f642a872996017ee7d4201a721d29919f8677e05
kamatil <karim.amahtil@gmail.com>
2025-03-07 02:14:25 +0000

    dev: disable signup if not allowed

--- middleware/table.ts
+++ middleware/table.ts
@@ -1,10 +1,15 @@
 export default defineNuxtRouteMiddleware(async (to) => {
 	const database = useState<Database>("database");
 	const table = useState<Table>("table");
-	const currentTableInPath = decodeURIComponent(
+	let currentTableInPath: string | undefined = decodeURIComponent(
 		(to.params.table as string) ||
 			to.path.split("/admin/tables/")[1]?.split("/")[0],
 	);
+
+	if (currentTableInPath === "undefined") currentTableInPath = undefined;
+
+	if (!currentTableInPath && to.path.replace(/\/$/, "").endsWith("/auth"))
+		currentTableInPath = "users";
 
 	if (
 		!database.value.tables ||
