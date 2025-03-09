027d64a1cd18ff50697ac02d495e067b79204bd4
kamatil <karim.amahtil@gmail.com>
2025-03-09 03:57:59 +0000

    fix: add support for non-string values

--- components/Column/Text.vue
+++ components/Column/Text.vue
@@ -1,7 +1,11 @@
 <template>
-    {{ value }}
+    <NPerformantEllipsis>
+        {{ value }}
+    </NPerformantEllipsis>
 </template>
 
 <script lang="ts" setup>
+import { NPerformantEllipsis } from "naive-ui";
+
 defineProps<{ value?: string | number }>();
 </script>
