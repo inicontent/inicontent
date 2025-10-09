847ef95a4ac035ad7b9459959507ac8fd2ef2300
kamatil <karim.amahtil@gmail.com>
2025-10-09 08:10:08 +0100

    refactor: update Asset.vue structure for improved organization and readability

--- app/components/Column/Asset.vue
+++ app/components/Column/Asset.vue
@@ -1,30 +1,33 @@
 <template>
-	<NFlex :wrap="false">
-		<template v-for="file in ([] as Asset[]).concat(value)">
-			<NTooltip v-if="file.type.startsWith('image/') || file.type === 'application/pdf'" :disabled="!file.name">
-				<template #trigger>
-					<div style="position: relative;">
-						<Icon v-if="file.type === 'application/pdf'" name="tabler:file-filled"
-							style="position: absolute;color: rgb(var(--primaryColor));top: 2px;" />
-						<NImage lazy
-							:src="file.type === 'application/pdf' ? `${file.publicURL}?fit=100` : file.publicURL"
-							:preview-src="file.type === 'application/pdf' ? `${file.publicURL}?raw` : file.publicURL"
-							:width="32" :height="32" object-fit="cover" style="height: 100%;"
-							:renderToolbar="(props) => renderToolbar(props, file)" />
-					</div>
-				</template>
-				{{ file.name }}
-			</NTooltip>
-			<NButton v-else tag="a" :href="file.publicURL" target="_blank" secondary type="primary" round>
-				<template #icon>
-					<NIcon>
-						<LazyAssetIcon :type="file.type" />
-					</NIcon>
-				</template>
-				{{ file.name }}
-			</NButton>
-		</template>
-	</NFlex>
+	<NImageGroup>
+		<NFlex :wrap="false">
+			<template v-for="file in ([] as Asset[]).concat(value)">
+				<NTooltip v-if="file.type.startsWith('image/') || file.type === 'application/pdf'"
+					:disabled="!file.name">
+					<template #trigger>
+						<div style="position: relative;">
+							<Icon v-if="file.type === 'application/pdf'" name="tabler:file-filled"
+								style="position: absolute;color: rgb(var(--primaryColor));top: 2px;" />
+							<NImage lazy
+								:src="file.type === 'application/pdf' ? `${file.publicURL}?fit=100` : file.publicURL"
+								:preview-src="file.type === 'application/pdf' ? `${file.publicURL}?raw` : file.publicURL"
+								:width="32" :height="32" object-fit="cover" style="height: 100%;"
+								:renderToolbar="(props) => renderToolbar(props, file)" />
+						</div>
+					</template>
+					{{ file.name }}
+				</NTooltip>
+				<NButton v-else tag="a" :href="file.publicURL" target="_blank" secondary type="primary" round>
+					<template #icon>
+						<NIcon>
+							<LazyAssetIcon :type="file.type" />
+						</NIcon>
+					</template>
+					{{ file.name }}
+				</NButton>
+			</template>
+		</NFlex>
+	</NImageGroup>
 </template>
 
 <script lang="ts" setup>
@@ -51,15 +54,15 @@ const renderToolbar: (
 	},
 	file?: Asset,
 ) => {
-		if (download.props && file?.publicURL)
-			download.props.onClick = (event: MouseEvent) => {
-				event?.preventDefault()
-				window.open(file.publicURL as string, "_blank")
-				close?.props?.onClick?.()
-			}
-		return [
-			file?.name
-				? h(
+	if (download.props && file?.publicURL)
+		download.props.onClick = (event: MouseEvent) => {
+			event?.preventDefault()
+			window.open(file.publicURL as string, "_blank")
+			close?.props?.onClick?.()
+		}
+	return [
+		file?.name
+			? h(
 					NTooltip,
 					{},
 					{
@@ -72,14 +75,14 @@ const renderToolbar: (
 							),
 					},
 				)
-				: null,
-			rotateCounterclockwise,
-			rotateClockwise,
-			zoomIn,
-			zoomOut,
-			resizeToOriginalSize,
-			download,
-			close,
-		]
-	}
+			: null,
+		rotateCounterclockwise,
+		rotateClockwise,
+		zoomIn,
+		zoomOut,
+		resizeToOriginalSize,
+		download,
+		close,
+	]
+}
 </script>
