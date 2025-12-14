19c971d5d37b17c06b68b1a4499035a62ee55fbf
kamatil <karim.amahtil@gmail.com>
2025-12-14 19:50:47 +0100

    refactor: replace localModelValue with modelValue for improved state management in Select component

--- app/components/Field/Select.vue
+++ app/components/Field/Select.vue
@@ -1,29 +1,25 @@
 <template>
-	<FieldWrapper :field :rule v-model="localModelValue">
-		<NSelect v-model:value="localModelValue" :placeholder="t(field.key)" :options :tag="!!field.custom"
+	<FieldWrapper :field :rule v-model="modelValue">
+		<NSelect v-model:value="modelValue" :placeholder="t(field.key)" :options :tag="!!field.custom"
 			max-tag-count="responsive" :consistent-menu-width="false" :multiple="!!field.isArray" filterable clearable
 			v-bind="field.inputProps
 				? typeof field.inputProps === 'function'
-					? field.inputProps(localModelValue) ?? {}
+					? field.inputProps(modelValue) ?? {}
 					: field.inputProps
 				: {}" />
 	</FieldWrapper>
 </template>
 
 <script lang="ts" setup>
-import { isArrayOfArrays, isArrayOfObjects } from "inibase/utils"
-import type { FormItemRule } from "naive-ui"
+import { isArrayOfArrays, isArrayOfObjects } from "inibase/utils";
+import type { FormItemRule } from "naive-ui";
 
-const { field } = defineProps<{ field: Field }>()
+const { field } = defineProps<{ field: Field }>();
 
-const modelValue = defineModel<string | string[]>()
-const localModelValue = ref<string | string[] | undefined>(modelValue.value)
-watch(
-	() => localModelValue.value,
-	(value) => {
-		modelValue.value = isArrayOfArrays(value) ? value.flat() : value
-	},
-)
+const modelValue = defineModel<string | string[]>();
+if (modelValue.value && isArrayOfArrays(modelValue.value))
+	modelValue.value = modelValue.value.flat();
+
 const rule: FormItemRule = {
 	trigger: "change",
 	type: field.isArray ? "array" : "any",
@@ -31,24 +27,24 @@ const rule: FormItemRule = {
 	min: field.isArray ? field.min : undefined,
 	max: field.isArray ? field.max : undefined,
 	validator: async () => {
-		await nextTick()
-		return fieldValidator(field, modelValue.value)
+		await nextTick();
+		return fieldValidator(field, modelValue.value);
 	},
-}
+};
 
 const options = computed(() =>
 	field.options
 		? isArrayOfArrays(field.options)
 			? (field.options as [string | number, string][]).map(([value]) => ({
-				value: value,
-				label: t(value),
-			}))
-			: isArrayOfObjects(field.options)
-				? field.options
-				: (field.options as string[]).map((value) => ({
 					value: value,
 					label: t(value),
 				}))
+			: isArrayOfObjects(field.options)
+				? field.options
+				: (field.options as string[]).map((value) => ({
+						value: value,
+						label: t(value),
+					}))
 		: [],
-)
+);
 </script>
