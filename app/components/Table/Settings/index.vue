9d6ef7eceacd6ba42a487e41086805c8460f4e66
kamatil <karim.amahtil@gmail.com>
2025-12-21 22:03:14 +0100

    fix: update NSelect show update handler to check for undefined value and improve code formatting

--- app/components/Table/Settings/index.vue
+++ app/components/Table/Settings/index.vue
@@ -56,7 +56,7 @@
 										<template #input="{ submit, deactivate }">
 											<NSelect ref="singleLabelSelect" size="small" clearable filterable show tag
 												:options="searchInSelectOptions" @update:value="submit($event)"
-												@update:show="(value) => value ? '' : deactivate()" />
+												@update:show="(value) => value === undefined ? '' : deactivate()" />
 										</template>
 										<template #trigger="{ activate, disabled }">
 											<NButton size="small" tertiary round
@@ -114,9 +114,9 @@
 </template>
 
 <script lang="ts" setup>
-import { flattenSchema, isArrayOfObjects, isNumber } from "inibase/utils"
-import type { FormInst } from "naive-ui"
-import { Icon, NTag } from "#components"
+import { flattenSchema, isArrayOfObjects, isNumber } from "inibase/utils";
+import type { FormInst } from "naive-ui";
+import { Icon, NTag } from "#components";
 
 onMounted(() => {
 	document.onkeydown = (e) => {
@@ -126,91 +126,91 @@ onMounted(() => {
 				(e.key.toLowerCase() === "s" || e.key === "س")
 			)
 		)
-			return
-		e.preventDefault()
-		updateTable()
-	}
-})
+			return;
+		e.preventDefault();
+		updateTable();
+	};
+});
 
-const expandedNames = ref<string[]>()
+const expandedNames = ref<string[]>();
 function pushToSchema(type: string) {
 	const newField = {
 		id: `temp-${randomID()}`,
 		key: "",
 		required: false,
 		...(handleSelectedSchemaType(type) as any),
-	}
-	if (tableCopy.value.schema?.[0]?.key === 'id')
-		tableCopy.value.schema?.splice(-2, 0, newField)
+	};
+	if (tableCopy.value.schema?.[0]?.key === "id")
+		tableCopy.value.schema?.splice(-2, 0, newField);
 	else {
-		if (!tableCopy.value.schema)
-			tableCopy.value.schema = []
-		tableCopy.value.schema.push(newField)
+		if (!tableCopy.value.schema) tableCopy.value.schema = [];
+		tableCopy.value.schema.push(newField);
 	}
-	expandedNames.value = [newField.id]
+	expandedNames.value = [newField.id];
 	setTimeout(
 		() => document.getElementById(`element-${newField.id}`)?.scrollIntoView(),
 		300,
-	)
+	);
 }
 
-const appConfig = useAppConfig()
-const Loading = useState<Record<string, boolean>>("Loading", () => ({}))
-const route = useRoute()
-const router = useRouter()
-const database = useState<Database>("database")
-const table = useState<Table>("table")
-const settingsFormRef = ref<FormInst>()
+const appConfig = useAppConfig();
+const Loading = useState<Record<string, boolean>>("Loading", () => ({}));
+const route = useRoute();
+const router = useRouter();
+const database = useState<Database>("database");
+const table = useState<Table>("table");
+const settingsFormRef = ref<FormInst>();
 const tableCopy = ref<
 	Table & {
-		localLabel?: { value: string; label: string }[]
+		localLabel?: { value: string; label: string }[];
 	}
 >({
 	...toRaw(table.value),
 	displayAs: table.value.displayAs || "table",
 	schema: table.value.schema || [],
-})
-const Language = useCookie<LanguagesType>("language", { sameSite: true })
+});
+const Language = useCookie<LanguagesType>("language", { sameSite: true });
 function removeTempIds(schema: Schema): Schema {
 	return schema.map((field) => {
-		if (field.id && String(field.id).startsWith("temp-")) delete field.id
+		if (field.id && String(field.id).startsWith("temp-")) delete field.id;
 
 		if (isArrayOfObjects(field.children)) {
 			return {
 				...field,
 				children: removeTempIds(field.children),
-			}
+			};
 		}
-		return field
-	})
+		return field;
+	});
 }
 
 const sessionID = useCookie<string | null>("sessionID", {
 	sameSite: true,
-})
+});
 
 async function updateTable() {
 	settingsFormRef.value?.validate(async (errors) => {
 		if (!errors) {
 			const bodyContent = (({ onRequest, onResponse, ...rest }) => ({
 				...rest,
-			}))(tableCopy.value)
-			Loading.value.updateTable = true
+			}))(tableCopy.value);
+			Loading.value.updateTable = true;
 
-			if (bodyContent.displayAs === "table") bodyContent.displayAs = "" as any
+			if (bodyContent.displayAs === "table") bodyContent.displayAs = "" as any;
 
 			if (bodyContent.localLabel)
 				bodyContent.label = bodyContent.localLabel
 					.map(({ value }: { value: string }) => value)
-					.join(" ")
+					.join(" ");
 
 			if (bodyContent.schema)
-				bodyContent.schema = removeTempIds(bodyContent.schema)
+				bodyContent.schema = removeTempIds(bodyContent.schema);
 
 			const data = await $fetch<
 				apiResponse<Table & { localLabel?: { value: string; label: string }[] }>
 			>(
-				`${appConfig.apiBase}inicontent/databases/${database.value.slug
+				`${appConfig.apiBase}inicontent/databases/${
+					database.value.slug
 				}/${route.params.table}`,
 				{
 					method: "PUT",
@@ -221,41 +221,42 @@ async function updateTable() {
 					},
 					credentials: "include",
 				},
-			)
+			);
 			const tableIndex = database.value.tables?.findIndex(
 				({ slug }) => slug === route.params.table,
-			)
+			);
 			if (
 				tableIndex !== undefined &&
 				tableIndex !== -1 &&
 				database.value.tables &&
 				data?.result
 			) {
-				data.result.displayAs = data.result.displayAs || "table"
-				database.value.tables[tableIndex] = data.result
-				table.value = data.result
-				tableCopy.value = data.result
+				data.result.displayAs = data.result.displayAs || "table";
+				database.value.tables[tableIndex] = data.result;
+				table.value = data.result;
+				tableCopy.value = data.result;
 
 				if (route.params.table !== data.result.slug)
 					router.replace({
 						params: { table: data.result.slug },
-					})
-				window.$message.success(data?.message ?? t("success"))
-			} else window.$message.error(data?.message ?? t("error"))
+					});
+				window.$message.success(data?.message ?? t("success"));
+			} else window.$message.error(data?.message ?? t("error"));
 
-			Loading.value.updateTable = false
-		} else window.$message.error(t("inputsAreInvalid"))
-	})
+			Loading.value.updateTable = false;
+		} else window.$message.error(t("inputsAreInvalid"));
+	});
 }
 
 const isUnDeletable = computed(() =>
 	["users", "pages", "blocks"].includes(table.value?.slug),
-)
+);
 
 async function deleteTable() {
-	Loading.value.deleteTable = true
+	Loading.value.deleteTable = true;
 	const data = await $fetch<apiResponse>(
-		`${appConfig.apiBase}inicontent/databases/${database.value.slug
+		`${appConfig.apiBase}inicontent/databases/${
+			database.value.slug
 		}/${route.params.table}`,
 		{
 			method: "DELETE",
@@ -265,32 +266,32 @@ async function deleteTable() {
 			},
 			credentials: "include",
 		},
-	)
+	);
 	if (data?.result) {
 		const tableIndex = database.value.tables?.findIndex(
 			({ slug }) => slug === route.params.table,
-		)
+		);
 		if (tableIndex !== undefined && tableIndex !== -1)
-			database.value.tables = database.value.tables?.toSpliced(tableIndex, 1)
-		Loading.value.deleteTable = false
-		window.$message.success(data?.message ?? t("success"))
+			database.value.tables = database.value.tables?.toSpliced(tableIndex, 1);
+		Loading.value.deleteTable = false;
+		window.$message.success(data?.message ?? t("success"));
 		setTimeout(
 			async () =>
 				await navigateTo(
 					`${route.params.database ? `/${database.value.slug}` : ""}/admin/tables`,
 				),
 			800,
-		)
-	} else window.$message.error(data?.message ?? t("error"))
-	Loading.value.deleteTable = false
+		);
+	} else window.$message.error(data?.message ?? t("error"));
+	Loading.value.deleteTable = false;
 }
 
 const flattenCopySchema = computed<Schema>(() =>
 	tableCopy.value.schema ? flattenSchema(tableCopy.value.schema as any) : [],
-)
-const singleLabelSelect = ref()
+);
+const singleLabelSelect = ref();
 function focusSingleLabelSelect() {
-	setTimeout(() => singleLabelSelect.value?.focusInput(), 200)
+	setTimeout(() => singleLabelSelect.value?.focusInput(), 200);
 }
 watch(
 	() => tableCopy.value.displayAs,
@@ -298,52 +299,52 @@ watch(
 		tableCopy.value.localLabel =
 			view !== "kanban"
 				? tableCopy.value.label
-					?.split(/(@\w+)/g)
-					.filter((value: string) => value.trim() != "")
-					.map((label: string) => {
-						if (label.startsWith("@"))
+						?.split(/(@\w+)/g)
+						.filter((value: string) => value.trim() !== "")
+						.map((label: string) => {
+							if (label.startsWith("@"))
+								return {
+									label:
+										flattenCopySchema.value.find(
+											({ id }) => String(id) === label.slice(1),
+										)?.key ?? "",
+									value: label,
+								};
 							return {
-								label:
-									flattenCopySchema.value.find(
-										({ id }) => String(id) === label.slice(1),
-									)?.key ?? "",
+								label,
 								value: label,
-							}
-						return {
-							label,
-							value: label,
-						}
-					})
-				: undefined
+							};
+						})
+				: undefined;
 	},
 	{ immediate: true },
-)
+);
 watch(
 	() => tableCopy.value?.localLabel,
 	(newValue) => {
 		if (newValue?.length) {
 			const localLabelWithNoEmptys = newValue.filter(
 				({ label }) => label.trim() !== "",
-			)
+			);
 			if (localLabelWithNoEmptys.length !== newValue.length)
-				tableCopy.value.localLabel = localLabelWithNoEmptys
+				tableCopy.value.localLabel = localLabelWithNoEmptys;
 		}
 	},
-)
+);
 function onAppendToLabel(label: string) {
-	if (isNumber(label) && !String(label).startsWith("@")) label = `@${label}`
+	if (isNumber(label) && !String(label).startsWith("@")) label = `@${label}`;
 	if (isNumber(label.slice(1)) && String(label).startsWith("@"))
 		return {
 			label:
 				flattenCopySchema.value.find(({ id }) => String(id) === label.slice(1))
 					?.key ?? "undefined",
 			value: label,
-		}
+		};
 
 	return {
 		label: label,
 		value: label,
-	}
+	};
 }
 function renderSingleLabel(
 	labelObject: { label: string; value: string },
@@ -354,25 +355,25 @@ function renderSingleLabel(
 		{
 			type:
 				labelObject.value.startsWith("@") &&
-					isNumber(labelObject.value.slice(1))
+				isNumber(labelObject.value.slice(1))
 					? "primary"
 					: "default",
 			closable: true,
 			round: true,
 			bordered: false,
 			onClose: () => {
-				tableCopy.value.localLabel?.splice(index, 1)
+				tableCopy.value.localLabel?.splice(index, 1);
 			},
 		},
 		{
 			default: () => labelObject.label,
 		},
-	)
+	);
 }
 
 const searchInSelectOptions = computed(() =>
 	generateSearchInOptions(table.value?.schema, undefined, true),
-)
+);
 
 const groupBySelectOptions = computed(() =>
 	generateSearchInOptions(
@@ -388,7 +389,7 @@ const groupBySelectOptions = computed(() =>
 			.map(({ id }) => String(id)),
 		true,
 	),
-)
+);
 
 const generalSettingsSchema = reactive<Schema>([
 	{
@@ -397,8 +398,8 @@ const generalSettingsSchema = reactive<Schema>([
 		required: true,
 		inputProps: ["users", "pages", "blocks"].includes(table.value?.slug)
 			? {
-				disabled: true,
-			}
+					disabled: true,
+				}
 			: {},
 		width: 3,
 	},
@@ -408,8 +409,8 @@ const generalSettingsSchema = reactive<Schema>([
 		subType: "icon",
 		inputProps: ["users", "pages", "blocks"].includes(table.value?.slug)
 			? {
-				disabled: true,
-			}
+					disabled: true,
+				}
 			: {},
 		width: 3,
 	},
@@ -425,19 +426,19 @@ const generalSettingsSchema = reactive<Schema>([
 		],
 		inputProps: ["users", "pages", "blocks"].includes(table.value?.slug)
 			? {
-				disabled: true,
-			}
+					disabled: true,
+				}
 			: {},
 		width: 3,
 	},
 	{
 		key: "compression",
-		label: t('compression.label'),
+		label: t("compression.label"),
 		type: "boolean",
 		inputProps: ["users", "pages", "blocks"].includes(table.value?.slug)
 			? {
-				disabled: true,
-			}
+					disabled: true,
+				}
 			: {},
 		width: 5,
 	},
@@ -446,8 +447,8 @@ const generalSettingsSchema = reactive<Schema>([
 		type: "boolean",
 		inputProps: ["users", "pages", "blocks"].includes(table.value?.slug)
 			? {
-				disabled: true,
-			}
+					disabled: true,
+				}
 			: {},
 		width: 5,
 	},
@@ -457,8 +458,8 @@ const generalSettingsSchema = reactive<Schema>([
 		description: "recentItemsAppearAtTheTop",
 		inputProps: ["users", "pages", "blocks"].includes(table.value?.slug)
 			? {
-				disabled: true,
-			}
+					disabled: true,
+				}
 			: {},
 		width: 5,
 	},
@@ -468,8 +469,8 @@ const generalSettingsSchema = reactive<Schema>([
 		description: "disableIdEncryption",
 		inputProps: ["users", "pages", "blocks"].includes(table.value?.slug)
 			? {
-				disabled: true,
-			}
+					disabled: true,
+				}
 			: {},
 		width: 5,
 	},
@@ -479,24 +480,24 @@ const generalSettingsSchema = reactive<Schema>([
 		description: "enableActivityLog",
 		width: 5,
 	},
-])
+]);
 
 function generateMentionOptions(
 	schema?: Schema,
 	prefix?: string,
 ): {
-	label: string
-	value: string | number
+	label: string;
+	value: string | number;
 }[] {
 	let RETURN: {
-		label: string
-		value: string | number
-	}[] = []
+		label: string;
+		value: string | number;
+	}[] = [];
 
-	if (!schema) return RETURN
+	if (!schema) return RETURN;
 
 	for (const field of schema) {
-		if (!field.id || field.id.toString().startsWith("temp-")) continue
+		if (!field.id || field.id.toString().startsWith("temp-")) continue;
 		if (
 			(Array.isArray(field.type) && field.type.includes("array")) ||
 			(field.type === "array" &&
@@ -504,20 +505,23 @@ function generateMentionOptions(
 				Array.isArray(field.children) &&
 				isArrayOfObjects(field.children))
 		)
-			continue
+			continue;
 		if (
 			field.children &&
 			Array.isArray(field.children) &&
 			isArrayOfObjects(field.children)
 		)
-			RETURN = [...RETURN, ...generateMentionOptions(field.children, field.key)]
+			RETURN = [
+				...RETURN,
+				...generateMentionOptions(field.children, field.key),
+			];
 		else
 			RETURN.push({
 				label: (prefix ? `${prefix}/` : "") + field.key,
 				value: field.id,
-			})
+			});
 	}
-	return RETURN
+	return RETURN;
 }
 
 const labelField: Field = {
@@ -527,23 +531,23 @@ const labelField: Field = {
 	options: generateMentionOptions(tableCopy.value?.schema),
 	inputProps: {
 		filter: (pattern: string, option: { value: string }) => {
-			if (!pattern) return true
+			if (!pattern) return true;
 			if (typeof option.value === "string") {
-				return option.value.startsWith(pattern)
+				return option.value.startsWith(pattern);
 			}
-			return pattern === option.value
+			return pattern === option.value;
 		},
 		type: "textarea",
 		autosize: {
 			minRows: 3,
 		},
 	},
-}
+};
 
 useHead({
 	title: `${t(database.value.slug)} | ${t(table.value?.slug)} : ${t("settings")}`,
 	link: [
 		{ rel: "icon", href: database.value?.icon?.publicURL ?? "/favicon.ico" },
 	],
-})
+});
 </script>
