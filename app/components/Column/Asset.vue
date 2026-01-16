a73123eacd6e013817f32d9e23ba4f8c10e56636
kamatil <karim.amahtil@gmail.com>
2026-01-16 22:44:14 +0100

    fix: adjust tooltip formatting and increase thumbnail size in Asset component

--- app/components/Column/Asset.vue
+++ app/components/Column/Asset.vue
@@ -2,11 +2,10 @@
 	<NImageGroup>
 		<NFlex :wrap="false" class="list">
 			<template v-for="file in ([] as Asset[]).concat(value)">
-				<NTooltip
-					:disabled="!file.name">
+				<NTooltip :disabled="!file.name">
 					<template #trigger>
-						<LazyAssetThumb :asset="file" :container-selector="$route.params.id ? undefined: '#DataTable'"
-							:size="20" />
+						<LazyAssetThumb :asset="file" :container-selector="$route.params.id ? undefined : '#DataTable'"
+							:size="40" />
 					</template>
 					{{ file.name }}{{ file.extension ? `.${file.extension}` : '' }}
 				</NTooltip>
