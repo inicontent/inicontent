0011b060d52f47825936195a1c8c802ec684b57e
Karim Amahtil <karim.amahtil@gmail.com>
2026-04-07 01:10:43 +0100

    fix: update import statements for consistency in Editor.vue

--- app/components/Field/Html/Editor.vue
+++ app/components/Field/Html/Editor.vue
@@ -345,12 +345,12 @@
 import { Color as TiptapColor } from "@tiptap/extension-color"
 import TiptapHighlight from "@tiptap/extension-highlight"
 import Link from "@tiptap/extension-link"
-import Table from "@tiptap/extension-table"
+import { Table } from "@tiptap/extension-table"
 import TableCell from "@tiptap/extension-table-cell"
 import TableHeader from "@tiptap/extension-table-header"
 import TableRow from "@tiptap/extension-table-row"
 import TiptapTextAlign from "@tiptap/extension-text-align"
-import TiptapTextStyle from "@tiptap/extension-text-style"
+import { TextStyle as TiptapTextStyle } from "@tiptap/extension-text-style"
 import TiptapUnderline from "@tiptap/extension-underline"
 
 const modelValue = defineModel<string>()
